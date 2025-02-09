f2fef193c48922b946abab6a2d25fdf07436314a
Sasha Karpovich <roobel2005@gmail.com>
2025-02-09 15:17:15 +0300

    feat(copy.js): implement copy function to copy files from source to destination folder fix(getPath.js): add missing newline for better code readability and style consistency

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import fs from 'fs';
+import path, {join} from 'path';
+import {getPath} from "../utils/getPath.js";
+
 const copy = async () => {
-    // Write your code here 
+    const {__dirname} = getPath(import.meta.url);
+    const sourceFolder = join(__dirname, 'files');
+    const destinationFolder = join(__dirname, 'files_copy');
+
+    if (!fs.existsSync(sourceFolder)) {
+        console.error('FS operation failed');
+        return;
+    }
+
+    if (fs.existsSync(destinationFolder)) {
+        console.error('FS operation failed');
+        return;
+    }
+    fs.mkdirSync(destinationFolder, { recursive: true });
+
+    const items = fs.readdirSync(sourceFolder);
+    items.forEach((item) => {
+        const sourceItem = path.join(sourceFolder, item);
+        const destinationItem = path.join(destinationFolder, item);
+
+        fs.copyFileSync(sourceItem, destinationItem);
+    });
 };
 
 await copy();
--- src/utils/getPath.js
+++ src/utils/getPath.js
@@ -4,5 +4,6 @@ import { dirname } from 'path';
 export function getPath(metaUrl) {
     const filename = fileURLToPath(metaUrl);
     const dir = dirname(filename);
+
     return { __dirname: dir, __filename: filename };
 }
