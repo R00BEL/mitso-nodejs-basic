d37b5df5781ef583517f0b5afb297fa0638d39ba
Sasha Karpovich <roobel2005@gmail.com>
2025-02-09 14:51:13 +0300

    feat(fs): add .gitignore file to exclude unnecessary files and directories from version control feat(fs): implement create function to write a fresh.txt file in a new 'files' directory feat(utils): add getPath utility function to retrieve directory and filename from module URL

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import {getPath} from "../utils/getPath.js";
+
 const create = async () => {
-    // Write your code here 
+    const {__dirname} = getPath(import.meta.url)
+    const folderPath = join(__dirname, 'files');
+    const filePath = join(folderPath, 'fresh.txt');
+
+    try {
+        await fs.writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+    } catch (err) {
+        if (err.code === 'EEXIST') {
+            return  console.error('FS operation failed');
+        }
+
+        console.error(err.message);
+    }
 };
 
 await create();
---
+++ src/utils/getPath.js
@@ -0,0 +1,8 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+export function getPath(metaUrl) {
+    const filename = fileURLToPath(metaUrl);
+    const dir = dirname(filename);
+    return { __dirname: dir, __filename: filename };
+}
