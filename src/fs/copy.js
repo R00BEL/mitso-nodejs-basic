c9ae12df7f0ec54e91c434c35b8ffd3ff3c12851
Sasha Karpovich <roobel2005@gmail.com>
2025-02-09 15:50:52 +0300

    refactor(copy.js): switch to fs/promises for asynchronous file operations to improve performance

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,30 +1,34 @@
-import fs from 'fs';
-import path, {join} from 'path';
-import {getPath} from "../utils/getPath.js";
+import fs from 'fs/promises';
+import path, { join } from 'path';
+import { getPath } from "../utils/getPath.js";
 
 const copy = async () => {
-    const {__dirname} = getPath(import.meta.url);
+    const { __dirname } = getPath(import.meta.url);
     const sourceFolder = join(__dirname, 'files');
     const destinationFolder = join(__dirname, 'files_copy');
 
-    if (!fs.existsSync(sourceFolder)) {
+    try {
+        await fs.access(sourceFolder);
+    } catch (err) {
         console.error('FS operation failed');
         return;
     }
 
-    if (fs.existsSync(destinationFolder)) {
+    try {
+        await fs.access(destinationFolder);
         console.error('FS operation failed');
         return;
+    } catch (err) {}
+
+    await fs.mkdir(destinationFolder, { recursive: true });
+
+    const items = await fs.readdir(sourceFolder);
+    for (const item of items) {
+        const sourceItem = join(sourceFolder, item);
+        const destinationItem = join(destinationFolder, item);
+
+        await fs.copyFile(sourceItem, destinationItem);
     }
-    fs.mkdirSync(destinationFolder, { recursive: true });
-
-    const items = fs.readdirSync(sourceFolder);
-    items.forEach((item) => {
-        const sourceItem = path.join(sourceFolder, item);
-        const destinationItem = path.join(destinationFolder, item);
-
-        fs.copyFileSync(sourceItem, destinationItem);
-    });
 };
 
 await copy();
