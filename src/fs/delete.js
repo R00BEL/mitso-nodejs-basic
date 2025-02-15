5a1e58d6273dcc2921b1c6a6989ced4e2b50a76e
Sasha Karpovich <roobel2005@gmail.com>
2025-02-15 12:35:41 +0300

    feat(delete.js): implement file deletion functionality to remove a specific file from the filesystem

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import {getPath} from "../utils/getPath.js";
+import {join} from "path";
+
 const remove = async () => {
-    // Write your code here 
+    const {__dirname} = getPath(import.meta.url)
+    const folderPath = join(__dirname, 'files');
+    const filePath = join(folderPath, 'fileToRemove.txt');
+
+    try {
+        await fs.unlink(filePath);
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await remove();
