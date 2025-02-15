f1bd09215c394a0270461eac197b5a5bb034325e
Sasha Karpovich <roobel2005@gmail.com>
2025-02-15 12:43:58 +0300

    feat(read.js): implement file reading functionality to read content from fileToRead.txt

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import {getPath} from "../utils/getPath.js";
+import {join} from "path";
+
 const read = async () => {
-    // Write your code here 
+    const {__dirname} = getPath(import.meta.url)
+    const folderPath = join(__dirname, 'files');
+    const filePath = join(folderPath, 'fileToRead.txt');
+
+    try {
+        const content = await fs.readFile(filePath, 'utf-8');
+        console.log(content);
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await read();
