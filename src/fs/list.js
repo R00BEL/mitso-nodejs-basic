1bb3aea45ae4ba73473fedf5559c4c00346584ae
Sasha Karpovich <roobel2005@gmail.com>
2025-02-15 12:40:01 +0300

    feat(list.js): implement file listing functionality to read and display files in the 'files' directory

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import {getPath} from "../utils/getPath.js";
+import {join} from "path";
+
 const list = async () => {
-    // Write your code here 
+    const {__dirname} = getPath(import.meta.url)
+    const folderPath = join(__dirname, 'files');
+
+    try {
+        const files = await fs.readdir(folderPath);
+
+        files.map(file => console.log(file))
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await list();
