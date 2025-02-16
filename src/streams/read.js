5bb8b16e1d8dac03a903cf0d65bf3218886c9960
Sasha Karpovich <roobel2005@gmail.com>
2025-02-16 11:21:05 +0300

    feat(read.js): implement file reading functionality to read and output file content

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import {getPath} from "../utils/getPath.js";
+import {join} from "path";
+
 const read = async () => {
-    // Write your code here 
+    const {__dirname} = getPath(import.meta.url)
+    const folderPath = join(__dirname, 'files');
+    const filePath = join(folderPath, 'fileToRead.txt');
+
+    const readStream = fs.createReadStream(filePath, 'utf8');
+
+    readStream.on('data', chunk => {
+        process.stdout.write(chunk);
+    });
 };
 
 await read();
