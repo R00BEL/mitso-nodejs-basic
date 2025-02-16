7c5a4771096d5a2895fb6eee6dba694e9882f029
Sasha Karpovich <roobel2005@gmail.com>
2025-02-16 11:54:15 +0300

    feat(write.js): implement file writing functionality using streams to allow user input to be saved to a file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import {getPath} from "../utils/getPath.js";
+import {join} from "path";
+
 const write = async () => {
-    // Write your code here 
+    const {__dirname} = getPath(import.meta.url)
+    const folderPath = join(__dirname, 'files');
+    const filePath = join(folderPath, 'fileToWrite.txt');
+
+    const writeStream = fs.createWriteStream(filePath);
+
+    process.stdin.pipe(writeStream);
 };
 
 await write();
