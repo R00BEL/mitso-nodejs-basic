2b2c1a02c94c09b6c10f8e79b7d80a7ff49bbfa1
Sasha Karpovich <roobel2005@gmail.com>
2025-02-17 14:05:17 +0300

    feat(decompress.js): implement file decompression functionality using streams and zlib to handle .gz files

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import {getPath} from "../utils/getPath.js";
+import {join} from "path";
+import fs from "fs";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    const {__dirname} = getPath(import.meta.url)
+
+    const folderPath = join(__dirname, 'files');
+    const inputPath = join(folderPath, 'archive.gz');
+    const outputPath = join(folderPath, 'fileToCompress1.txt');
+
+    const readStream = fs.createReadStream(inputPath);
+    const writeStream = fs.createWriteStream(outputPath);
+    const gunzip = zlib.createGunzip();
+
+    readStream.pipe(gunzip).pipe(writeStream);
 };
 
 await decompress();
