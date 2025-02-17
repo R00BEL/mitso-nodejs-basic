21a1e4f91e677a9ef484b874c8f8366aaa6db545
Sasha Karpovich <roobel2005@gmail.com>
2025-02-17 11:31:34 +0300

    feat(compress.js): implement file compression functionality using zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import {getPath} from "../utils/getPath.js";
+import {join} from "path";
+import fs from 'fs';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const {__dirname} = getPath(import.meta.url)
+
+    const folderPath = join(__dirname, 'files');
+    const inputPath = join(folderPath, 'fileToCompress.txt');
+    const outputPath = join(folderPath, 'archive.gz');
+
+    const readStream = fs.createReadStream(inputPath);
+    const writeStream = fs.createWriteStream(outputPath);
+    const gzip = zlib.createGzip();
+
+    readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
