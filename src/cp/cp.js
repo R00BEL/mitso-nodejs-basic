24fa2ff69a74af0063911f4589d65d634f3578d7
Sasha Karpovich <roobel2005@gmail.com>
2025-02-18 11:00:24 +0300

    feat(cp.js): implement spawnChildProcess function to execute a child process with arguments

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { spawn } from 'child_process';
+import {getPath} from "../utils/getPath.js";
+import {join} from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const { __dirname } = getPath(import.meta.url);
+    const sourceFolder = join(__dirname, 'files');
+    const filePatch = join(sourceFolder, 'script.js');
+
+    const childProcess = spawn('node', [filePatch, ...args]);
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ["someArgument1", "someArgument2"]);
